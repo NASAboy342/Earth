2bd2fd604821e7bf2cbaac7dd8b88e91481dd2ce
PinSopheaktra <sopheakpin342@gmail.com>
2025-08-30 17:34:33 +0700

    use a for navigeting the page

--- Earth/src/layouts/MainLayout.vue
+++ Earth/src/layouts/MainLayout.vue
@@ -29,8 +29,8 @@ watch(
     </div>
     <header>
       <nav>
-        <router-link to="/">Home</router-link>
-        <router-link to="/about">About</router-link>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
       </nav>
     </header>
 
